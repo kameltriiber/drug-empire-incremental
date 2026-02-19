fc18d23e1008649810f6aa315f21592b5bfbb39f
kameltriiber <91786467+kameltriiber@users.noreply.github.com>
2026-02-19 01:31:08 +0100

    balance changes

--- weeds.js
+++ weeds.js
@@ -4,22 +4,22 @@ window.weeds = {
         base_cost: 10,
         price: 1,
         cost_mult: 1.1,
-        income: 1,
+        income: 0.3,
     },
 
     california: {
         name: "California Weed",
-        base_cost: 100,
+        base_cost: 120,
         price: 5,
-        cost_mult: 1.2,
-        income: 1,
+        cost_mult: 1.125,
+        income: 0.9,
     },
 
     special: {
         name: "Special Weed",
-        base_cost: 1000,
+        base_cost: 1500,
         price: 25,
-        cost_mult: 1.3,
-        income: 0.5,
+        cost_mult: 1.145,
+        income: 0.45,
     }
 };
